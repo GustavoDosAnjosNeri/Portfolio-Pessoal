7793cc7f69f34d785c1f6262f63d5ff824620567
Gustavo dos Anjos Neri <gustavodosanjosneri@gmail.com>
2022-01-14 17:17:02 -0300

    1st Push
    Base do portfolio

---
+++ header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css\fixeds.css">
+</head>
+<body>
+    <header>
+        <div class="esq">
+            <h3>KelliNeri</h3>
+        </div>
+        <div class="mid">
+        </div>
+        <div class="dir">
+
+        </div>
+    </header>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,53 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css\body.css">
+    <title>Document</title>
+</head>
+<?php
+    include("header.php");
+    ?>
+<body>
+    <section class="sct1 container">
+        <div class="items ">
+            <img src="assets\images\img1.png" alt="">
+            <div class="info">
+                <h3>Goleiro gay gay goleiro</h3>
+                <p>Lorem ipsum dolor Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi, nemo impedit. Cupiditate delectus unde odit molestias modi. Quasi voluptatibus aut modi, sint eveniet a dolore aliquam quod est. Sunt, pariatur! sit amet consectetur adipisicing elit. Incidunt blanditiis similique, odit possimus dolores pariatur vel inventore. Rerum, amet. Reprehenderit officiis inventore ea porro, non ipsam quisquam amet eveniet earum?</p>
+            </div>
+        </div>
+    </section>
+    <div class="div1">
+    </div>
+    <section class="sct2 container">
+        <div class="items">
+            <div class="info">
+                <h3>Cala a boca monkey</h3>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique repellendus, libero voluptate quod vel provident esse ab a, veniam recusandae, qui accusantium voluptas alias aut illo sapiente expedita quisquam. Eaque!locale_filter_matches</p>
+            </div>
+            <img src="assets\images\img2.png" alt="">
+        </div>
+    </section>
+    <div class="div2">
+
+    </div>
+    <section class="sct3 container">
+        <div class="items">
+            <div class="images">
+                <img class="side1" src="assets\images\img3.png" alt="">
+                <img class="side2" src="assets\images\img4.png" alt="">
+            </div>
+            <div class="info">
+                <h1>LOREM MACAQUITUS SAOUD</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore saepe esse doloremque. Blanditiis, ipsam enim? Voluptate id officia officiis. Illum corrupti nesciunt natus reprehenderit accusantium facere unde rem corporis consectetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium explicabo aspernatur libero cum nihil vel quis nostrum, facere hic esse corrupti dolores, veritatis, asperiores amet? Totam quia velit et porro. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa quia sint dignissimos possimus voluptatum laboriosam, necessitatibus labore ea vitae ex quod voluptatibus vel dolores, ducimus nulla nobis architecto laborum natus.</p>
+            </div>        </div>
+    </section>
+</body>
+    
+</html>
+
+
